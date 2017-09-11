73c3cc59a9190a1d350f444c846ed41e43339ba4
anmaslov <lelick29@gmail.com>
2017-09-11 14:30:54 +0300

    change version

--- install/version.php
+++ install/version.php
@@ -1,5 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.0.0",
-    "VERSION_DATE" => "2017-05-11 15:00:00");
+    "VERSION" => "1.1.0",
+    "VERSION_DATE" => "2017-09-11 15:00:00"
+);
--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -4,6 +4,7 @@ $MESS ['DHL_MODULE_NAME'] = "Стоимость доставки DHL";
 $MESS ['DHL_MODULE_DESCRIPTION'] = "Добавляет возможность производить расчет стоимости доставки DHL";
 $MESS ['DHL_PARTNER_NAME'] = "Маслов Алексей";
 $MESS ['DHL_PARTNER_URI'] = "https://github.com/anmaslov";
+
 $MESS ['DHL_INSTALL_TITLE'] = "Установка модуля расчета доставки DHL";
 $MESS ['DHL_UNINSTALL_TITLE'] = "Деинсталляция модуля расчета доставки DHL";
 $MESS ['DHL_INSTALL_SUCCESS'] = "Модуль расчета стоимости DHL успешно установлен";
