20a65f3ecd302f371c366757fe80e151bfdc580e
Maslov Aleksey <lelick29@gmail.com>
2018-01-11 00:07:05 +0300

    change version

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.1.0",
-    "VERSION_DATE" => "2017-09-11 15:00:00"
+    "VERSION" => "1.2.0",
+    "VERSION_DATE" => "2018-01-11 01:00:00"
 );
